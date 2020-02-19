ef1c82da2e07cf47067ed24bbd027d47bd1e6b58
Farrukhbek <farruhzokirov00@gmail.com>
2020-02-19 15:35:44 +0500

    adding bootstrap for data. closes #14

--- pet-clinic-data/src/main/java/com/jafton/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/jafton/sfgpetclinic/services/map/PetServiceMap.java
@@ -1,11 +1,11 @@
 package com.jafton.sfgpetclinic.services.map;
 
 import com.jafton.sfgpetclinic.model.Pet;
-import com.jafton.sfgpetclinic.services.CrudService;
+import com.jafton.sfgpetclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
 
     @Override
     public Set<Pet> findAll() {
--- pet-clinic-data/src/main/java/com/jafton/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/jafton/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package com.jafton.sfgpetclinic.services.map;
 
 import com.jafton.sfgpetclinic.model.Vet;
-import com.jafton.sfgpetclinic.services.CrudService;
+import com.jafton.sfgpetclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 
     @Override
     public Set<Vet> findAll() {
