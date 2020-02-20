605d927631dc408e40699916e11bc3c15ec19c3c
Farrukhbek <farruhzokirov00@gmail.com>
2020-02-20 17:32:40 +0500

    added PetTypeService closes #29

---
+++ pet-clinic-data/src/main/java/com/jafton/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,8 @@
+package com.jafton.sfgpetclinic.services;
+
+import com.jafton.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+
+}
---
+++ pet-clinic-data/src/main/java/com/jafton/sfgpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,36 @@
+package com.jafton.sfgpetclinic.services.map;
+
+import com.jafton.sfgpetclinic.model.PetType;
+import com.jafton.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        deleteById(id);
+    }
+}
