7ab0e3619136b34123811f5b9946c0c8b92d9718
Farrukhbek <farruhzokirov00@gmail.com>
2020-02-27 15:46:29 +0500

    added json endpoint

--- pet-clinic-web/src/main/java/com/jafton/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/jafton/sfgpetclinic/controllers/VetController.java
@@ -1,9 +1,15 @@
 package com.jafton.sfgpetclinic.controllers;
 
+import com.jafton.sfgpetclinic.model.Vet;
 import com.jafton.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
+
 //
 @Controller
 public class VetController {
@@ -20,5 +26,12 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+
+        return vetService.findAll();
     }
 }
